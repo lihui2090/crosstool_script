50f77e9b23ff5a2ae96b05bc3c7eb08d82731949
ll <ll@debian.(none)>
2016-08-08 09:38:15 -0400

    # crosstool_script #this is for s3c2440 #use version crosstool-ng-1.19.0.tar.bz2 #config file backup olds3c2440-config #src file some is download by hand #finish compile ct-ng add ct-ng path to .hashrc #finish compile crosstool add arm tool path to .bashrc
    the src code in /home/ll/src
    
    by the .config file
    the file need download
    can download by hand in the dir
    
    binutils-2.19.1a.tar.bz2
    gdb-6.8a.tar.bz2
    ltrace_0.5.3.orig.tar.gz
    dmalloc-5.5.2.tgz
    glibc-2.9.tar.bz2
    mpfr-2.4.2.tar.xz
    duma_2_5_15.tar.gz
    glibc-ports-2.9.tar.bz2
    ncurses-5.9.tar.gz
    ecj-latest.jar
    gmp-4.3.2.tar.bz2
    strace-4.5.19.tar.bz2
    expat-2.1.0.tar.gz
    libelf-0.8.13.tar.gz
    gcc-4.3.2.tar.bz2
    linux-2.6.32.61.tar.xz
    
    the most part is right ,
    the cross_gdb have error
    COMPILE ....................

--- build_crosstool_for_s3c2240.sh
+++ build_crosstool_for_s3c2240.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#for s3c2440
+#kernel 2.6.32
+#pc 686
+
 CROSSTOOL_NG_PATH=/opt/crosstool-ng
 CROSSTOOL_NG_BIN_PATH=/opt/crosstool-ng/crosstool-ng_bin
 CROSSTOOL_NG_VERSION=1.19.0
@@ -22,12 +26,11 @@ build_s3c2440_crosstool(){
 sudo mkdir -p $CROSSTOOL_S3C_BUILD_PATH
 sudo chown ll $CROSSTOOL_S3C_BUILD_PATH
 sudo chgrp ll $CROSSTOOL_S3C_BUILD_PATH
-
 cp ./config/oldconfig-s3c2440  $CROSSTOOL_NG_PATH/crosstool-ng-$CROSSTOOL_NG_VERSION/.config
 cp ./config/oldconfig-s3c2440 $CROSSTOOL_S3C_BUILD_PATH/.config
-cd $CROSSTOOL_S3C_BUILD_PATH  
+cd $CROSSTOOL_S3C_BUILD_PATH
 ct-ng menuconfig
 ct-ng build
-
 }
+
 build_s3c2440_crosstool
--- enviroment.sh
+++ enviroment.sh
@@ -1,1 +1,3 @@
-apt-get install readelf objdump objcopy bzip2 gzip  gcc gperf bison  flex  wget  libncurses5-dev
+#!/bin/bash
+#exapt libexapt for compile cross-gdb
+apt-get install readelf objdump objcopy bzip2 gzip  gcc gperf bison  flex  wget  libncurses5-dev expat  libexpat-dev
