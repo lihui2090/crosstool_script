f0e003b013869588434b399cac33d8a53952eb8b
bob <3219823514@qq.com>
2016-10-20 01:58:20 -0400

    add host==target ==arm tools chains  make script

---
+++ build_crosstool_on_s3c2240.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#for s3c2440
+#kernel 2.6.32
+#pc 686
+
+CROSSTOOL_NG_PATH=/opt/crosstool-ng
+CROSSTOOL_NG_BIN_PATH=/opt/crosstool-ng/crosstool-ng_bin
+CROSSTOOL_NG_VERSION=1.19.0
+CROSSTOOL_S3C_BUILD_PATH=$CROSSTOOL_NG_PATH/build_crosstool_on_s3c
+build_example(){
+
+mkdir /a/directory/to/build/your/toolchain
+cd /a/directory/to/build/your/toolchain
+ct-ng help
+ct-ng list-samples
+ct-ng show-arm-unknown-linux-gnueabi
+#once you know what sample to use, configure ct-ng to use it:
+ct-ng arm-unknown-linux-gnueabi
+#samples are configured to install in "${HOME}/x-tools/arm-unknown-linux-gnueabi" by default. This should be OK for a first time user, so you can now build your toolchain:
+ct-ng build
+
+}
+
+
+build_s3c2440_crosstool(){
+sudo mkdir -p $CROSSTOOL_S3C_BUILD_PATH
+sudo chown ll $CROSSTOOL_S3C_BUILD_PATH
+sudo chgrp ll $CROSSTOOL_S3C_BUILD_PATH
+cp ./config/targethost_s3c2440_config  $CROSSTOOL_NG_PATH/crosstool-ng-$CROSSTOOL_NG_VERSION/.config
+cp ./config/targethost_s3c2440_config $CROSSTOOL_S3C_BUILD_PATH/.config
+cd $CROSSTOOL_S3C_BUILD_PATH
+ct-ng menuconfig
+ct-ng build
+}
+
+build_s3c2440_crosstool
