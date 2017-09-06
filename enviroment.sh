d6260d30700f0236b963803210cae9042d9466fa
shenme <shenme@debian>
2017-09-06 02:11:21 -0400

    change apt pkg

--- enviroment.sh
+++ enviroment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #exapt libexapt for compile cross-gdb
-apt-get install readelf objdump objcopy bzip2 gzip  gcc gperf bison  flex  wget  libncurses5-dev expat  libexpat-dev
+apt-get install libtool libtool-bin  bzip2 gzip  gcc gperf bison  flex  wget  libncurses5-dev expat  libexpat-dev
