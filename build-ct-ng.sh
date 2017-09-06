369e4a0f298fcec3bc618b6e548d5b36618d7877
shenme <shenme@debian>
2017-09-06 02:10:43 -0400

    change download way

--- build-ct-ng.sh
+++ build-ct-ng.sh
@@ -6,21 +6,23 @@ CROSSTOOL_NG_VERSION=1.19.0
 
 build_ct-ng(){
 sudo mkdir -p  $CROSSTOOL_NG_PATH
-sudo chown ll  ${CROSSTOOL_NG_PATH}
-sudo chgrp ll  ${CROSSTOOL_NG_PATH}
+sudo chown shenme  ${CROSSTOOL_NG_PATH}
+sudo chgrp shenme  ${CROSSTOOL_NG_PATH}
 sudo mkdir  -p  $CROSSTOOL_NG_BIN_PATH
-sudo chown  ll $CROSSTOOL_NG_BIN_PATH
-sudo chgrp ll  $CROSSTOOL_NG_BIN_PATH
+sudo chown  shenme $CROSSTOOL_NG_BIN_PATH
+sudo chgrp shenme  $CROSSTOOL_NG_BIN_PATH
 cd $CROSSTOOL_NG_PATH
 echo "http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-${CROSSTOOL_NG_VERSION}.tar.bz2" 
+if [ ! -e crosstool-ng-$CROSSTOOL_NG_VERSION.tar.bz2 ] ; then
 wget "http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-${CROSSTOOL_NG_VERSION}.tar.bz2"
+fi 
 tar xjf crosstool-ng-$CROSSTOOL_NG_VERSION.tar.bz2
 cd crosstool-ng-$CROSSTOOL_NG_VERSION
 ./configure  --prefix=$CROSSTOOL_NG_BIN_PATH
 make
 make install
 export PATH="${PATH}:$CROSSTOOL_NG_BIN_PATH/bin"
-echo  "export PATH=$PATH:/opt/crosstool-ng/crosstool-ng_bin/bin" > /home/ll/.bashrc
+echo  "export PATH=$PATH:/opt/crosstool-ng/crosstool-ng_bin/bin" > /home/shenme/.bashrc
 ct-ng 
 }
 
